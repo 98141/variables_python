e35371efef314d5d966ceb36a0fbf47261113691
ARMANDO MORA <66850932+98141@users.noreply.github.com>
2022-11-27 13:10:15 -0500

    Add files via upload

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -25,6 +25,7 @@ palabra_2 = str(input('Ingrese palabra 2:'))
 # Crear una variable llamada palabra_combinada
 # con los recortes solicitados de las variables
 # palabra_1 y palabra_2 en el orden correspondiente
-
+palabra_combinada = palabra_1[0:3] + palabra_2[0:2]
 
 # Imprima en pantalla la variable palabra_combinada
+print(palabra_combinada)
